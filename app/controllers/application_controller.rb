944df6c3567414354210f343171e506f08b0ebe1
railsfactory-saravanakumar <saravanakumar@railsfactory.org>
2013-11-05 12:19:23 +0530

    code optimized

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   def authentication
-    unless user_signed_in?
-      redirect_to "/users/auth/facebook"
-    end
+    redirect_to "/users/auth/facebook" unless user_signed_in?
+  end
+  
+  def after_sign_in_path_for(resource_or_scope)
+    new_photo_path
   end
   
 end
